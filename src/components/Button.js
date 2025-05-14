cbcfa62b73db3cdffa50f6fee8495fcaf6c91737
ksahai <ksahai@opentext.com>
2025-05-14 15:26:57 +0530

    add button list

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({name}) => {
+  return (
+    
+        <button className='px-5 py-2 rounded-lg m-2 bg-gray-200  hover:bg-gray-300 whitespace-nowrap'>{name}</button>
+    
+  )
+}
+
+export default Button
--- src/components/Buttonlist.js
+++ src/components/Buttonlist.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Button from './Button'
 
 const Buttonlist = () => {
+
+  const list=["All","Music","Mixes","Amitabh Bhattacharya","T-Series","Live","Kriti Sanon","Selena Gomez","Interscope Rounds","Coldplay","News"
+    ,"Gulshan Kumar","Dua Lipa","Podcasts","Soul Music"
+  ]
+
   return (
-    <div>Buttonlist</div>
+    <div>
+    <div className='flex overflow-x-auto space-x-2'>
+      {
+        list.map((item)=>{
+          return <Button name={item}/>
+        })
+      }
+    
+    </div>
+    </div>
+    
   )
 }
 
